1ba7436fbbf24319a1a3c9c4c34b44b04f3f2a12
Lauren Grubb <lauren.elyse.grubb@gmail.com>
2022-07-20 20:47:25 -0400

    ran through the hw and followed the instructions

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-let MONGODB_URI = "mongodb://127.0.0.1:27017/plantsDatabase";
+let MONGODB_URI = "mongodb+srv://cluster0.cvjaw0c.mongodb.net/plantsDatabase";
 
 mongoose
   .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
